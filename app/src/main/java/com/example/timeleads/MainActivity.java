6a257042ad05fc7029d9609e03cc149162ed9dec
barthkiol <gabrielbarth22@gmail.com>
2022-05-11 22:56:47 -0300

    Fragment com pageview

--- app/src/main/java/com/example/timeleads/MainActivity.java
+++ app/src/main/java/com/example/timeleads/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.timeleads;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.annotation.SuppressLint;
 import android.graphics.Color;
@@ -16,6 +18,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 public class MainActivity extends AppCompatActivity {
 
     BottomNavigationView bottomNav;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,33 +26,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.GRAY));
 
-        getSupportFragmentManager().beginTransaction().replace(R.id.linearLayout, new Events()).commit();
+        //getSupportFragmentManager().beginTransaction().replace(R.id.linearLayout, new Events()).commit();
 
         bottomNav = findViewById(R.id.bottom_navigation);
-        bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @SuppressLint("ResourceType")
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                Fragment fragment = null;
-
-                switch (item.getItemId()) {
+        viewPager = findViewById(R.id.view_pager);
+        setUpViewPager();
+        bottomNav.setOnNavigationItemSelectedListener((item) ->{
+                switch (item.getItemId()){
                     case R.id.events:
-                        fragment = new Events();
+                        viewPager.setCurrentItem(0);
                         break;
                     case R.id.hours:
-                        fragment = new Hours();
+                        viewPager.setCurrentItem(1);
                         break;
                     case R.id.profile:
-                        fragment = new Profile();
+                        viewPager.setCurrentItem(2);
                         break;
                     case R.id.requests:
-                        fragment = new Requests();
+                        viewPager.setCurrentItem(3);
                         break;
                 }
-
-                getSupportFragmentManager().beginTransaction().replace(R.id.linearLayout, fragment).commit();
                 return true;
-            }
         });
 
 
@@ -57,5 +54,40 @@ public class MainActivity extends AppCompatActivity {
 
     public void setActionBarTitle(String title){
         getSupportActionBar().setTitle(title);
+    }
+
+    private void setUpViewPager() {
+        ViewPagerAdpater viewPagerAdpater = new ViewPagerAdpater(getSupportFragmentManager(), FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        viewPager.setAdapter(viewPagerAdpater);
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                    switch (position){
+                        case 0:
+                            bottomNav.getMenu().findItem(R.id.events).setChecked(true);
+                            break;
+                        case 1:
+                            bottomNav.getMenu().findItem(R.id.hours).setChecked(true);
+                            break;
+                        case 2:
+                            bottomNav.getMenu().findItem(R.id.profile).setChecked(true);
+                            break;
+                        case 3:
+                            bottomNav.getMenu().findItem(R.id.requests).setChecked(true);
+                            break;
+                    }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/timeleads/ViewPagerAdpater.java
@@ -0,0 +1,37 @@
+package com.example.timeleads;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+
+public class ViewPagerAdpater extends FragmentStatePagerAdapter {
+    public ViewPagerAdpater(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0:
+                return new Events();
+            case 1:
+                return new Hours();
+            case 2:
+                return new Profile();
+            case 3:
+                return new Requests();
+            default:
+                return new Events();
+
+        }
+
+    }
+
+    @Override
+    public int getCount() {
+        return 4;
+    }
+}
